b394d5428fe64c4ac4cf1bca56f10d9f2c839828
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-03 09:17:13 -0600

    Create test for add specialty method

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -13,5 +13,14 @@ describe Person do
 
     expect(kathy.name).to eq('Kathy Chan')
     expect(kathy.experience).to eq(10)
+    expect(kathy.specialties).to eq([])
+  end
+
+  it 'can add specialties' do
+    kathy = Person.new('Kathy Chan', 10)
+
+    kathy.add_specialty(:astrophysics)
+    kathy.add_specialty(:quantum_mechanics)
+    expect(kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
   end
 end
