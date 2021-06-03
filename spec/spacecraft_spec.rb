be1af81ededa92a2ad452007c12071b78c0b739a
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-03 09:08:15 -0600

    Create exists and has attributes tests for spacecraft

---
+++ spec/spacecraft_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require './lib/spacecraft'
+
+describe Spacecraft do
+  it 'exists' do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus).to be_a(Spacecraft)
+  end
+
+  it 'has attributes' do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus.name).to eq('Daedalus')
+    expect(daedalus.fuel).to eq(400)
+  end
+end
